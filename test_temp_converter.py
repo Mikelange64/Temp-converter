d09225d039b39cad6232230e1c18a1f7da1cd710
Mikelange <mikelange64@gmail.com>
2026-01-22 17:26:19 -0500

    Initial commit

---
+++ test_temp_converter.py
@@ -0,0 +1,14 @@
+import unittest
+from temp_converter import celsius_to_fahrenheit, fahrenheit_to_celsius
+
+class TestTempConverter(unittest.TestCase):
+    def test_celsius_to_fahrenheit(self):
+        self.assertEqual(celsius_to_fahrenheit(0), 32)
+        self.assertEqual(celsius_to_fahrenheit(100), 212)
+    
+    def test_fahrenheit_to_celsius(self):
+        self.assertAlmostEqual(fahrenheit_to_celsius(32), 0)
+        self.assertAlmostEqual(fahrenheit_to_celsius(212), 100)
+
+if __name__ == "__main__":
+    unittest.main()
